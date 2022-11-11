912d64448f1f37df684fc6dc53124e134190cce4
Wendryo <wendryo.sales@gmail.com>
2022-11-11 14:01:57 -0300

    feat: :sparkles: Ratings

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,28 @@
-# Requisito 10
+from tech_news.database import get_collection
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    query = [
+        {
+            "$group": {
+                "_id": "$title",
+                "comments_count": {"$sum": "$comments_count"},
+                "url": {"$first": "$url"},
+            }
+        },
+        {"$sort": {"comments_count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    response = list(get_collection().aggregate(query))
+    result = [(item["_id"], item["url"]) for item in response]
+    return result
 
 
-# Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    query = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    response = list(get_collection().aggregate(query))
+    return [item["_id"] for item in response]
